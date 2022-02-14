12d51565fd9d205e14e127aaf351fa4635d27ed5
Tarathep63130500055 <tarathep.siripis@mail.kmutt.ac.th>
2022-02-14 16:05:23 +0700

    Update Lastest Before Midterm

---
+++ basic-dynamic-style/src/class-binding-object-array-G2.vue
@@ -0,0 +1,29 @@
+<script setup>
+const isSucceed = false
+const msg = 'abc'
+const hasLength = () => msg.length !== 0
+console.log(hasLength())
+</script>
+
+<template>
+  <!-- class attribute with object syntax -->
+  <p :class="{ 'success-text': hasLength(), 'decor-text': isSucceed }">
+    this is class binding with object syntax
+  </p>
+  <!-- class attribute with array syntax -->
+  <p :class="['error-text', isSucceed ? 'decor-text' : '']">
+    this class binding with array syntax
+  </p>
+</template>
+
+<style>
+.success-text {
+  color: green;
+}
+.error-text {
+  color: red;
+}
+.decor-text {
+  text-decoration: underline;
+}
+</style>
---
+++ basic-dynamic-style/src/class-binding-object-array-G3.vue
@@ -0,0 +1,48 @@
+<script setup>
+const isEmpty = false
+const hasDecor = true
+const isSucceed = false
+const errorStyle = 'error-text'
+const decorStyle = 'decor-text'
+</script>
+
+<template>
+  <div>
+    <!-- class attribute with object syntax -->
+    <p :class="{ 'success-text': isSucceed }">
+      this applied success-text class
+    </p>
+    <p :class="{ 'success-text': isSucceed, 'decor-text': hasDecor }">
+      this applied success-text and class
+    </p>
+    <!-- class attribute with array syntax -->
+    <p :class="[errorStyle, decorStyle]">
+      This applied style stored in variable
+    </p>
+    <p :class="isSucceed ? 'success-text' : 'error-text'">
+      This applied style under condition
+    </p>
+    <p :class="['decor-text', isSucceed ? 'success-text' : 'error-text']">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quia amet
+      aspernatur, quo inventore autem consequatur incidunt. Laudantium minus
+      ipsam obcaecati dolorum deleniti reiciendis quibusdam architecto totam
+      voluptatem incidunt! Voluptas!
+    </p>
+  </div>
+</template>
+
+<style>
+.success-text {
+  color: green;
+}
+.error-text {
+  color: red;
+  font-style: italic;
+}
+.warning-text {
+  color: orange;
+}
+.decor-text {
+  text-decoration: overline;
+}
+</style>
